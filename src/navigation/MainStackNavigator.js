04ca79dc590a67e58fa65b0153771dc679a899ce
Altaf Hussain <altafhussain19@gmail.com>
2020-03-30 16:04:14 +0530

    Custom Navigation Drawer UI

--- src/navigation/MainStackNavigator.js
+++ src/navigation/MainStackNavigator.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
+import {View, Text, StyleSheet} from 'react-native';
 
 import HomeScreen from '../screens/Home';
 import DetailsScreen from '../screens/Details';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+function DrawerContent(props) {
+  const {navigation} = props.navigation;
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}>
+      <View
+        style={{
+          justifyContent: 'flex-end',
+          padding: 15,
+          height: 150,
+          backgroundColor: '#f1f1f1',
+          zIndex: 1,
+        }}>
+        <Text style={{fontWeight: 'bold', fontSize: 16}}>Altaf Hussain</Text>
+      </View>
+      <DrawerContentScrollView
+        {...props}
+        style={{backgroundColor: '#ffffff', marginTop: -40}}>
+        <View>
+          <DrawerItem
+            label="Home"
+            onPress={() => navigation.navigate('Home')}
+          />
+          <DrawerItem
+            label="Details"
+            onPress={() => navigation.navigate('Details')}
+          />
+        </View>
+      </DrawerContentScrollView>
+    </View>
+  );
+}
 
 function RootNavigator() {
   return (
@@ -47,10 +88,19 @@ function RootNavigator() {
 export default function MainStackNavigator() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator
+        drawerContent={navigation => <DrawerContent navigation={navigation} />}>
         <Drawer.Screen name="Home" component={RootNavigator} />
-        <Drawer.Screen name="Details" component={DetailsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+// const styles = StyleSheet.create({
+//   drawerContent: {
+//     flex: 1,
+//   },
+//   drawerSection: {
+//     marginTop: 15,
+//   },
+// });
